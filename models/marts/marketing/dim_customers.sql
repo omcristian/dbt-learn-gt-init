e6c32c68faad6ad9476964f1389c00b63330859d
Cristian Perficient Laptop <jcom.sur.movil@gmail.com>
2026-03-10 18:42:57 -0300

    fct_orders

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -3,27 +3,21 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+    -- select * from {{ ref('stg_jaffle_shop__orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
-
 ),
-
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -33,7 +27,6 @@ final as (
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
-
     left join customer_orders using (customer_id)
 
 )
--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -2,4 +2,5 @@ select
         id as customer_id,
         first_name,
         last_name 
-    from raw.jaffle_shop.CUSTOMERS
+    from raw.jaffle_shop.customers
+    
